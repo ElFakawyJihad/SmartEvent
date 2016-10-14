5376c07e9a71238efcf4ccb42279af684c8b62bf
MANSOUR EL FAKAWY Jihad <jihad3394@hotmail.fr>
2016-10-15 01:31:59 +0200

    Update accueil.js

--- accueil.js
+++ accueil.js
@@ -4,5 +4,6 @@ var server = http.createServer(function(req, res) {
   res.writeHead(200);
   res.end('Salut tout le monde !');
 });
+//process.env.PORT est le port d'écoute 
 server.listen(process.env.PORT);
 
