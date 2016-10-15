1ca7acf2daca965ff0296cbd75968c6e75aabd5b
Jihad <jihad394@hotmail.fr>
2016-10-16 00:33:07 +0200

    Accueil1

--- accueil1.js
+++ accueil1.js
@@ -1,19 +1,18 @@
 var http = require('http');
 
 var server = http.createServer(function(req, res) {
-  //Ecrire dans le Head,retour OK et type du renvoie
-  res.writeHead(200,{"Content-Type": "text/html"});
-  res.write('<!DOCTYPE html>'+
-  	'<html>'+
-  		'<head>'+
-  			'<meta charset="utf-8" />'+
-  			'<title>Ma page Node.js !<title>'+
-  			'</head>'+
-  			'<body>'+
-  				'<p>Voici un paragraphe <strong>HTML</strong> !</p>'+
-  				'</body>'+
-  			'</html>');
-  res.end();
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write('<!DOCTYPE html>'+
+'<html>'+
+'    <head>'+
+'        <meta charset="utf-8" />'+
+'        <title>Ma page Node.js !</title>'+
+'    </head>'+ 
+'    <body>'+
+'     	<p>Voici un paragraphe <strong>HTML</strong> !</p>'+
+'    </body>'+
+'</html>');
+    res.end();
 });
 //process.env.PORT est le port d'écoute 
 server.listen(process.env.PORT);
