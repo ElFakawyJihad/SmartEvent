1d5c076c74532cc9ac1d8f86224a016a498898de
Jihad <jihad394@hotmail.fr>
2016-10-16 11:34:46 +0200

    accueil

--- accueil1.js
+++ accueil1.js
@@ -4,7 +4,15 @@ var server = http.createServer(function(req, res) {
 	var page=url.parse(req.url).pathname;
 	console.log(page);
     res.writeHead(200, {"Content-Type": "text/html"});
-    res.write('Bien le Bonjour');
+    if (page=='/test'){
+    res.write('Bien le Bonjour Test');
+    }
+    else if (page=='/plage'){
+     res.write('On est dans la plage')
+    }
+    else {
+    res.write("Le Reste")	
+    }
     res.end();
 });
 //process.env.PORT est le port d'écoute 
