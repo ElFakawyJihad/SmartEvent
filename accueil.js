a05683e1286c1beaa997a903f2147d230e98cb3c
Antonin Durey <dureyantonin@gmail.com>
2016-11-01 23:19:37 +0100

    Premier succès pour lancer du code avec REST. Connection presque fini. Manque le cas où la connection échoue + les tests

--- accueil.js
+++ accueil.js
@@ -1,9 +1,46 @@
-var http = require('http');
+var express = require('express');
+var app = express();
+var fs = require("fs");
+var bodyParser = require('body-parser');
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Salut tout le monde !');
+var config = require("config");
+
+var databaseConfig = config.get("database");
+
+var mysql = require("mysql");
+
+
+
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.get('/', function(req, res){
+	res.send("Hello World !");
 });
-//process.env.PORT est le port d'écoute 
-server.listen(process.env.PORT);
+
+app.post('/connection', function (req, res) {
+  console.log("Email : " + req.body.email)
+	console.log("Password : " + req.body.password)
+
+	var email = req.body.email;
+	var password = req.body.password;
+
+	var connection = mysql.createConnection(databaseConfig);
+	connection.connect();
+	connection.query("SELECT * FROM user where email = '" + email + "' AND password = '" + password + "'", function(err, rows, fields){
+		if (rows.length) {
+	    res.end(JSON.stringify(rows[0]));
+		} else {
+			return "NO RESULT FOUND";		
+		}
+	});
+});
+
+var server = app.listen(8081, function () {
+   var host = server.address().address;
+   var port = server.address().port;
+
+   console.log("Server running at http://%s:%s", host, port)
+});
 
