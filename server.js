2f3a6b7190f447e47ccb320e698e3f80e001abe7
Antonin Durey <dureyantonin@gmail.com>
2016-12-06 19:39:07 +0100

    Suite et fin de la création d'un évènement

--- server.js
+++ server.js
@@ -112,28 +112,34 @@ app.post('/create_event', function (req, res) {
 	var client = new pg.Client(connectionString);
 	client.connect();
 
-	var query = client.query("INSERT into lieu(name, longitude, latitude) VALUES('" + localisation + "', '" + longitude + "', '" + latitude + "')'", function(err, result){
-		if(err){
-			res.write(JSON.stringify({message: "KO", error:err}));
-		} else {
-			res.write(JSON.stringify({message: "OK"}));
-		}	
-		res.end();	
+	var query = client.query("INSERT into lieu(name, longitude, latitude) VALUES('" + localisation + ", " + longitude + ", " + latitude + ")", function(err1, result1){
+    	if (err1) {
+            res.write(JSON.stringify({message: "KO", error:err1}));
+            res.end();
+
+        } else {
+
+        	var querybis = client.query("SELECT id FROM lieu WHERE name = '" + localisation + "' AND longitude = " + longitude + " AND latitude = " + latitude, function(err2, result2){
+        		if(err2){
+        			res.write(JSON.stringify({message: "KO", error:err2}));
+        			res.end();
+
+        		} else {
+        			var queryter = client.query("INSERT into event(titre, description, date_debut, nb_places, lieu_id) VALUES('" + titre + "', '" + description + "', '" + date_debut + "', " + capacity + ", " + result2 + ")", function(err3, result3){
+        				// , titre VARCHAR(45), description VARCHAR(255), photo VARCHAR(45), date_debut TIMESTAMP, date_fin TIMESTAMP, nb_places INT, lieu_id INT, organisateur_id INT
+
+        				if(err3){
+        					res.write(JSON.stringify({message: "KO", error:err3}));
+        				} else {
+        					res.write(JSON.stringify({message: "OK"}));
+        				}
+        				res.end();
+        			});
+        		}
+        	});
+        }
 	});
 
-/*
-	var query = client.query("INSERT into event(titre, description, date_debut, nb_places, ) VALUES('" + prenom + "', nom = '" + nom + "', naissance = '" + naissance + "', genre = '" + genre + "' WHERE email = '" + email + "'", function(err, result){
-		if(err){
-			res.write(JSON.stringify({message: "OK"}));
-		} else {
-			res.write(JSON.stringify({message: "KO"}));
-		}	
-		res.end();	
-	});
-
-	*/
-
-	
 });
 
 app.get('/test', function(req, res){
@@ -147,13 +153,35 @@ app.get('/test', function(req, res){
 
 	var client = new pg.Client(connectionString);
 	client.connect();
-	var query = client.query("INSERT INTO users (email, password) VALUES ('toto', 'tata')", function(err, result) {
-        	if (err) {
-                res.write(JSON.stringify({message: "KO", error:err}));
-            } else {
-				res.write(JSON.stringify({message: "OK"}));
-            }
-		res.end();
+	var query = client.query("INSERT into lieu(name, longitude, latitude) VALUES('Au M5, '50.2', '0.3280')", function(err1, result1){
+    	if (err1) {
+            res.write(JSON.stringify({message: "KO", error:err1}));
+            res.end();
+
+        } else {
+
+        	var querybis = client.query("SELECT id FROM lieu WHERE name = 'Au M5' AND longitude  = 50.2 AND latitude = 0.3280", function(err2, result2){
+        		if(err2){
+        			res.write(JSON.stringify({message: "KO", error:err2}));
+        			res.end();
+
+        		} else {
+        			var queryter = client.query("INSERT into event(titre, description, date_debut, nb_places, lieu_id) VALUES('This is a nice event', 'This is the description', '2016-12-07', '5', " + result2 + ")", function(err3, result3){
+        				// , titre VARCHAR(45), description VARCHAR(255), photo VARCHAR(45), date_debut TIMESTAMP, date_fin TIMESTAMP, nb_places INT, lieu_id INT, organisateur_id INT
+
+        				if(err3){
+        					res.write(JSON.stringify({message: "KO", error:err3}));
+        				} else {
+        					res.write(JSON.stringify({message: "OK"}));
+
+        				}
+        				res.end();
+
+        			});
+
+        		}
+        	});
+        }
 	});
 
 });
