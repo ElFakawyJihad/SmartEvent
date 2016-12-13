75edee461845a366c2ae81110c43d27828e84e99
Durey Antonin <durey@b10p7.fil.univ-lille1.fr>
2016-12-13 16:21:14 +0100

    Correction du script de création bis

--- helper_function.js
+++ helper_function.js
@@ -39,7 +39,7 @@ module.exports = {
 
 				this.none("INSERT INTO coming VALUES(1, 'Invité'), (2,'Viendra'), (3, 'Peut-être'), (4, 'Ne viendra pas')"),
 
-            	this.none("INSERT INTO users VALUES('dureyantonin@gmail.com', 'Antonin', 'Durey', '', '1995-01-17', 'azerty01', '0', '0'), ('test', 'test', 'test', '', '1970-01-01', 'test', '0', '0')"),
+            	this.none("INSERT INTO users VALUES('dureyantonin@gmail.com', 'Antonin', 'Durey', '', '1995-01-17', 'azerty01', '0', '0'), ('test', 'test', 'test', '', '1970-01-01', 'test', '0', '0')")
 
 
     	})
